0d8696b5be64bc66227ae09ec6e7d64e5be7e71d
Mochimucin <Mochimucin@users.noreply.github.com>
2023-11-20 17:40:18 +0800

    setup firebase in our web app and create login button for sign with popup

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,7 +1,4 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 import {
   getAuth,
   signInWithRedirect,
@@ -9,7 +6,6 @@ import {
   GoogleAuthProvider,
 } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAxyypgt55cyezPsuT92x_DpTkSUmGfluQ",
   authDomain: "crwn-clothing-db-42a95.firebaseapp.com",
@@ -19,7 +15,6 @@ const firebaseConfig = {
   appId: "1:139230743074:web:c760a9fdd7be40690a187b",
 };
 
-// Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
 const provider = new GoogleAuthProvider();
@@ -27,6 +22,12 @@ const provider = new GoogleAuthProvider();
 provider.setCustomParameters({
   prompt: "select_account",
 });
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  console.log(userAuth);
+};
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
